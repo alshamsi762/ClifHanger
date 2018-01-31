9f2b10d25b7cd404998057e408a3c9cffe037b21
Andrew Peterson <peter343@purdue.edu>
2018-01-31 11:37:26 -0500

    Proof-of-concept testing
    Small testing for class communication to ensure correct implementation.

---
+++ test.js
@@ -0,0 +1,8 @@
+/** Testing Class Communication **/
+
+var player1 = new Player(0, 100, "A1", null, null, "Andrew");
+var player2 = new Player(1, 100, "A2", null, null, "Amjad");
+
+function test() {
+
+}
