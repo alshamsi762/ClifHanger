95b030540e0e2a20cae5dab1f0ba09d942717f1f
Andrew Peterson <peter343@purdue.edu>
2018-01-31 11:18:59 -0500

    Created Item class
    Created Item class containing the constructor for item objects.

---
+++ item.js
@@ -0,0 +1,13 @@
+/** Item Object & Functions **/
+
+class Item {
+
+  // Constructor for Item Object
+  constructor(name, type, attributes, rarity) {
+    this.name = name;
+    this.type = type;
+    this.attributes = attributes;
+    this.rarity = rarity;
+  }
+  
+}
