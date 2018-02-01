ab68b88a53e88d333e67ede5bee6b025c670b381
Andrew Peterson <peter343@purdue.edu>
2018-01-31 19:12:45 -0500

    Change in items class

--- item.js
+++ item.js
@@ -3,11 +3,14 @@
 class Item {
 
   // Constructor for Item Object
-  constructor(name, type, attributes, rarity) {
+  constructor(name, itemType, attackType, range, damage, rarity) {
     this.name = name;
-    this.type = type;
-    this.attributes = attributes;
+    this.itemType = itemType;
+    this.attackType = attackType; // 0-basic, 1-radius
+    this.range = range; // 1, 2
+    this.damage = damage; // number
     this.rarity = rarity;
+    // Description
   }
-  
+
 }
