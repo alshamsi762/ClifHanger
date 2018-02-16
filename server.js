2952c7c4359b7dd2974ebcdba8b20c2aa9a66f69
Andrew Peterson <peter343@purdue.edu>
2018-02-16 12:18:53 -0500

    Looking into multiplayer

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ var express = require('express');
 
 /** Global Vars **/
 var app = express();
+var port = 2200;
 
 /** Main Thread **/
+app.set('port', 2200);
+
 // Send Index.html
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );;
 })
 
 // Set Port & Listen (Heroku Support)
-var port = 2200
 app.listen(port);
